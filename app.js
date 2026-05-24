d7495608f15845b19e40e92d1df4e86b603a92c1
Style9999 <bedoali9999@gmail.com>
2026-05-24 19:41:17 +0300

    Added dark and white mode, fixed scrolling

--- app.js
+++ app.js
@@ -7,8 +7,34 @@ const movieLists = document.querySelectorAll(".movie-list")
 
 arrows.forEach((arrow,i)=>{
     const itemNumber = movieLists[i].querySelectorAll("img").length;
+    let clickCounter = 0;
         arrow.addEventListener("click",() => {
-            movieLists[i].style.transform = `translateX(${movieLists[i].computedStyleMap()
+            clickCounter ++;
+            if (itemNumber -(3+clickCounter)> 0)
+            {
+                 movieLists[i].style.transform = `translateX(${movieLists[i].computedStyleMap()
                 .get("transform")[0].x.value-430}px)`;
+            }
+            else {
+                 movieLists[i].style.transform = "translateX(0)"
+                 clickCounter = 0;
+            }
+           
         });
+
+        console.log(movieLists[i].querySelectorAll("img").length);
 });
+
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle,.item-container h3,body");
+
+ball.addEventListener("click", ()=> {
+    items.forEach(item=>{
+
+        item.classList.toggle("active")
+    })
+
+
+})
+
